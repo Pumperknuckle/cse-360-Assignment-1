a0c5ca8ea3dd88536282c9ecf3cb565a1a9faceb
steph <steph@SGB-Laptop>
2020-10-05 23:36:18 -0700

    adds comments

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,61 @@
 package cse360assignment02;
-
+/**
+ * Stephen Bailey, CSE 360, Assignment 1
+ * AddingMachine.java
+ * 
+ * The AddingMachine programallows for adding or subtracting based on input
+ *
+ * @author 		Stephen Bailey
+ * @version		1.1
+ * @since		2020-10-5
+ */
 public class AddingMachine {
   private int total;
-  
+  /**
+   * The constructor AddingMachine initializes total variable
+   *
+   * @param  		none
+   * @return      	none
+   */
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
-  
+  /**
+   * The method getTotal gets current total 
+   *
+   * @return      	int total
+   */
   public int getTotal () {
     return 0;
   }
-  
+  /**
+   * The method add adds passed value to total
+   *
+   * @param			int value
+   * @return      	int total
+   */
   public void add (int value) {
   }
-
+  /**
+   * The method subtract subtracts passed value to total
+   *
+   * @param			int value
+   * @return      	int total
+   */
   public void subtract (int value) {
   }
- 
+  /**
+   * The method toString prints the history of changes
+   *
+   * @return      	String string
+   */
   public String toString () {
     return "";
   }
-
+  /**
+   * The method removes history
+   *
+   */
   public void clear() {
   }
 }
